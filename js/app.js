d15b85efab6766f0ddb8951418228e0f477128f3
Yaoning Wu - C15502 <Yaoning.Wu@microchip.com>
2018-06-01 11:34:14 -0700

    feat: implemented the functional game.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,109 @@
 /*
- * Create a list that holds all of your cards
+ * Global constants and variables
  */
-
+const CARD_CLASS_LIST = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb",
+                         "fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
+const STAR_NUMBER = 3;
+let gameStarted = false;
+let openedCards = [];
+let moveNumber = 0;
+let timeCounter = 0;
+let solvedCount = 0;
+let timeOutSetting;
 
 /*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ * Basic game flow
  */
+document.addEventListener('DOMContentLoaded', function () {
+    vex.defaultOptions.className = 'vex-theme-top';
+    initGame();
+});
+
+function initGame(){
+    createCardTable(shuffle(CARD_CLASS_LIST));
+    createStars(STAR_NUMBER);
+    document.querySelector(".restart").addEventListener("click", resetGame);
+    document.querySelectorAll(".card").forEach(function(card){
+        card.addEventListener("click", function(event) {
+            card = event.target;
+            // start game on first click
+            if (!gameStarted) {
+                gameStarted = true;
+                timeCounter = 0;
+                timeOutSetting = setTimeout(startTimer, 1000);
+            }
+
+            // if card can be flipped
+            if (openedCards.length < 2){
+                console.log(card);
+                card.classList.toggle("open");
+                card.classList.toggle("show");
+                console.log(card);
+                openedCards.push(card);
+            }
+            // check if cards match
+            if (openedCards.length === 2){
+                checkCardMatch();
+            }
+        });
+    });
+}
+
+function checkCardMatch() {
+    if (getCardClass(openedCards[0]) === getCardClass(openedCards[1])){
+        solvedCount++;
+        openedCards.forEach(function(card){
+            animateCard(card, "rubberBand");
+            card.classList.toggle("match");
+        });
+    } else {
+        openedCards.forEach(function(card){
+            animateCard(card, "shake");
+        });
+    }
+    openedCards = [];
+    countMove();
+    console.log("current solved: " + solvedCount);
+    if (solvedCount === 8){
+        endGame();
+    }
+}
+
+// reset game
+function resetGame(){
+    document.querySelector("ul.deck").innerHTML = "";
+    document.querySelector("ul.stars").innerHTML = "";
+    // Update Move Number Display
+    moveNumber = -1;
+    countMove();
+    openedCards = [];
+    timeCounter = 0;
+    solvedCount = 0;
+    gameStarted = false;
+    clearTimeout(timeOutSetting);
+    document.querySelector("#timer").innerHTML = 0;
+    // initialize game
+    initGame();
+}
+
+// game over
+function endGame(){
+    clearTimeout(timeOutSetting);
+    // show prompt
+    let stars = document.querySelectorAll(".fa-star").length;
+    vex.dialog.confirm({
+        message: `Congrats! You won the game with ${moveNumber} moves and ${stars}/3 star rating. Do you want to play again?`,
+        callback: function(value){
+            if (value){
+                resetGame();
+            }
+        }
+    });
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -25,14 +116,58 @@ function shuffle(array) {
     return array;
 }
 
-
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * Creation functions
  */
+function createCardTable(cardClassList) {
+    for (cardClass of cardClassList) {
+        createCardElement(cardClass);
+    }
+}
+
+function createCardElement(cardClass) {
+    document.querySelector("ul.deck").insertAdjacentHTML('beforeend', `<li class="card"><i class="fa ${cardClass}"></i></li>`);
+}
+
+function createStars(numOfStars) {
+    for (let i = 0; i < numOfStars; i++) {
+        document.querySelector("ul.stars").insertAdjacentHTML('beforeend', '<li class="card"><i class="fa fa-star"></i></li>');
+    }
+}
+
+function getCardClass(card){
+    return card.firstChild.className;
+}
+
+function animateCard(card, animateType) {
+    card.classList.add("animated", "infinite", animateType);
+	setTimeout(function () {
+        card.classList.remove("open", "show", "animated", "infinite", animateType);
+	}, 600);
+}
+
+// starts the timer
+function startTimer(){
+    timeCounter += 1;
+    document.querySelector("#timer").innerHTML = timeCounter;
+    timeOutSetting = setTimeout(startTimer, 1000);
+}
+
+// move count
+function countMove(){
+    moveNumber += 1;
+    document.querySelector(".moves").innerHTML = moveNumber;
+    updateStarRating(moveNumber);
+}
+
+// reduce star rating
+function updateStarRating(moveNumber){
+    let stars = document.querySelectorAll(".fa-star");
+
+    if (stars.length === 1) { return; }
+
+    if ((moveNumber - (STAR_NUMBER - stars.length) * 10 === 10)) {
+        stars[stars.length - 1].classList.toggle("fa-star");
+        stars[stars.length - 1].classList.toggle("fa-star-o");
+    } 
+}
